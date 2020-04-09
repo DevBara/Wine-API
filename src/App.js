676ba56d8e3ccc2d22e21ed792ba2739241174aa
DevBara <61251493+DevBara@users.noreply.github.com>
2020-04-08 21:49:25 -0500

    successful push

--- src/App.js
+++ src/App.js
@@ -8,14 +8,32 @@ export default class App extends Component {
     this.state ={
       data: []
     }
+
+    axios.get('http://myapi-profstream.herokuapp.com/api/ee686f/wines')
+    .then(response => {
+      console.log(response)
+    })
+
+
+    axios.post('http://myapi-profstream.herokuapp.com/api/ee686f/wines',{
+      "name": "test",
+      "year": 2009,
+      "grapes": "sour",
+      "country": "USA",
+      "region": "Triple D",
+      "description": "The aromas of fruit and spice give one a hint of the light drinkability of this lovely wine, which makes an excellent complement to fish dishes.",
+      "picture": "",
+      "price": 23,
+
+    })
+      .then(response => {
+        console.log(response);
+      }) 
+      
   }
 
-  async componentDidMount() {
-    const response = await axios.get('http://myapi-profstream.herokuapp.com/api/ee686f/wines')
-    console.log(response);
-  } catch(e){
-    console.log(e);
-  }
+  
+
 
   render() {
     return (
