441a39d8a693d5385706182e217243765e0ce8c9
DevBara <61251493+DevBara@users.noreply.github.com>
2020-04-11 21:45:52 -0500

    delete HTTP

--- src/App.js
+++ src/App.js
@@ -6,16 +6,24 @@ export default class App extends Component {
     super(props);
 
     this.state ={
-      data: []
+      data: [],
     }
+  }
 
-    axios.get('http://myapi-profstream.herokuapp.com/api/ee686f/wines')
+    getWineList(){
+      axios.get('http://myapi-profstream.herokuapp.com/api/ee686f/wines')
     .then(response => {
+      this.setState({data: response.data })
       console.log(response)
     })
+    .catch(error => {
+      console.log(error);
+    })
+  }
 
 
-    axios.post('http://myapi-profstream.herokuapp.com/api/ee686f/wines',{
+    postNewWine() {
+      axios.post('http://myapi-profstream.herokuapp.com/api/ee686f/wines',{
       "name": "test",
       "year": 2009,
       "grapes": "sour",
@@ -24,11 +32,21 @@ export default class App extends Component {
       "description": "The aromas of fruit and spice give one a hint of the light drinkability of this lovely wine, which makes an excellent complement to fish dishes.",
       "picture": "",
       "price": 23,
-
     })
       .then(response => {
         console.log(response);
-      }) 
+      }) .catch(error =>{
+          console.log(error);
+      })
+
+      deleteWine(){
+        axios. delete('http://myapi-profstream.herokuapp.com/api/ee686f/wine')
+      }
+
+    componentDidMount(){
+      this.getWineList();
+    }
+ 
       
   }
 
@@ -36,15 +54,18 @@ export default class App extends Component {
 
 
   render() {
+    const {data}=this.state;
+    
     return (
       <div className="parent">
         <h1>Wine List</h1>
-        <p>Use the form to input new wines</p>
+        <p>Look here: {data}</p>
+        {/* <p>Use the form to input new wines</p>
         <form action="https://myapi-profstream.herokuapp.com/api/ee686f/wines">
           <input type ="text" name="description" placeholder="wine name" />
           
           <button type="submit">Submit</button>
-        </form>
+        </form> */}
         
       </div>
     )
