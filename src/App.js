ea550e843dbb4bfab4bc8604f4c72bb82d8da3c5
DevBara <61251493+DevBara@users.noreply.github.com>
2020-04-12 13:33:50 -0500

    created form to input new wine

--- src/App.js
+++ src/App.js
@@ -30,6 +30,9 @@ export default class App extends Component {
       .then(response => {
         console.log(response.data)
         console.log(response)
+        this.setState({
+          wines:response.data
+        })
       }).catch(error =>{
         console.log(error)
       })
@@ -43,7 +46,33 @@ export default class App extends Component {
     return (
       <div>
         
+        <p>Use the form to input new wines</p>
+        <form action="https://myapi-profstream.herokuapp.com/api/ee686f/wines">
+          <input type ="text" name="description" placeholder="wine name" />
+          <input type ="text" placeholder="Name of Wine" />
+          <input type ="id" placeholder="ID" />
+          <select type ="text" name="years">
+              <option>Wine Year</option> 
+              <option>2000</option>
+              <option>2001</option>
+              <option>2002</option>
+              <option>2003</option>
+              <option>2004</option>
+              <option>2005</option>
+              <option>2006</option>
+              <option>2007</option>
+          </select>
+          <input type ="text" placeholder="Grape Type" />
+          <input type ="text" placeholder="Country" />
+          <input type ="text" placeholder="Region" />
+          <input type ="text" placeholder="Description" />
+          <input type ="text" placeholder="Price" />
+
+          <button type="submit">Submit New Wine</button>
+          <button type="submit">Delete A Wine</button>
+        </form>
       </div>
+      
     )
   }
 }
